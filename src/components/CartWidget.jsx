ebd6cbd557adcefed55e87448aead2eaf5a70cd2
sistemaschoho <sistemas@chohocolombia.com>
2021-09-02 23:41:37 -0500

    Segundo desafio con NavBar y components

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+const CartWidget = () => {
+    return (
+        <div>
+            <a>
+                <img className="w-25" src="https://i.pinimg.com/originals/bf/19/90/bf1990e247292b756ca7926443f00ab6.jpg" alt=""/>
+            </a>
+        </div>
+    )
+}
+
+export default CartWidget
